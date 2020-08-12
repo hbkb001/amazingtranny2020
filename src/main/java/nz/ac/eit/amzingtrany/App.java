b24e367bab64570e382bb67e9cc2b949f24ad1d5
Bryce <currab1@student.eit.ac.nz>
2020-08-12 19:57:26 +1200

    Number Lists are full (1-30)

--- src/main/java/nz/ac/eit/amzingtrany/App.java
+++ src/main/java/nz/ac/eit/amzingtrany/App.java
@@ -12,11 +12,68 @@ public class App
 {
     public static void main( String[] args )
     {
-        // Need To Finish adding the French & German numbers up to "30"
+
         List<String> frenchTranslations=new ArrayList<>();
-        frenchTranslations.add("UN");
+       // 1
+        frenchTranslations.add("Un");
+        // 2
         frenchTranslations.add("Deux");
+        // 3
         frenchTranslations.add("Trois");
+        //4
+        frenchTranslations.add("Quatre");
+        //5
+        frenchTranslations.add("Cinq");
+        //6
+        frenchTranslations.add("Six");
+        //7
+        frenchTranslations.add("Sept");
+        //8
+        frenchTranslations.add("Huit");
+        //9
+        frenchTranslations.add("Neuf");
+        //10
+        frenchTranslations.add("Dix");
+        //11
+        frenchTranslations.add("Onze");
+        //12
+        frenchTranslations.add("Douze");
+        //13
+        frenchTranslations.add("Treize");
+        //14
+        frenchTranslations.add("Quatorze");
+        //15
+        frenchTranslations.add("Quinze");
+        //16
+        frenchTranslations.add("Seize");
+        //17
+        frenchTranslations.add("Dix-Sept");
+        //18
+        frenchTranslations.add("Dix-Huit");
+        //19
+        frenchTranslations.add("Dix-Neuf");
+        //20
+        frenchTranslations.add("Vingt");
+        //21
+        frenchTranslations.add("Vingt-et-Un");
+        //22
+        frenchTranslations.add("Vingt-Deux");
+        //23
+        frenchTranslations.add("Vingt-Trois");
+        //24
+        frenchTranslations.add("Vingt-Quatre");
+        //25
+        frenchTranslations.add("Vingt-Cinq");
+        //26
+        frenchTranslations.add("Vingt-Six");
+        //27
+        frenchTranslations.add("Vingt-Sept");
+        //28
+        frenchTranslations.add("Vingt-Huit");
+        //29
+        frenchTranslations.add("Vingt-Neuf");
+        //30
+        frenchTranslations.add("Trente");
 
 
         List<String> germanTranslations=new ArrayList<>();
@@ -26,8 +83,59 @@ public class App
         germanTranslations.add("Zwei");
         // 3
         germanTranslations.add("Drei");
-// finish in here
-       //30
+        //4
+        germanTranslations.add("vier");
+        //5
+        germanTranslations.add("fünf");
+        //6
+        germanTranslations.add("sechs");
+        //7
+        germanTranslations.add("sieben");
+        //8
+        germanTranslations.add("acht");
+        //9
+        germanTranslations.add("neun");
+        //10
+        germanTranslations.add("zehn");
+        //11
+        germanTranslations.add("elf");
+        //12
+        germanTranslations.add("zwölf");
+        //13
+        germanTranslations.add("dreizehn");
+        //14
+        germanTranslations.add("vierzehn");
+        //15
+        germanTranslations.add("fünfzehn");
+        //16
+        germanTranslations.add("sechzehn");
+        //17
+        germanTranslations.add("siebzehn");
+        //18
+        germanTranslations.add("achtzehn");
+        //19
+        germanTranslations.add("neunzehn");
+        //20
+        germanTranslations.add("zwanzig");
+        //21
+        germanTranslations.add("einundzwanzig");
+        //22
+        germanTranslations.add("zweiundzwanzig");
+        //23
+        germanTranslations.add("dreiundzwanzig");
+        //24
+        germanTranslations.add("vierundzwanzig");
+        //25
+        germanTranslations.add("fünfundzwanzig");
+        //26
+        germanTranslations.add("sechsundzwanzig");
+        //27
+        germanTranslations.add("siebenundzwanzig");
+        //28
+        germanTranslations.add("achtundzwanzig");
+        //29
+        germanTranslations.add("neunundzwanzig");
+        //30
         germanTranslations.add("dreißig");
 
         System.out.println( "What Is The Number To Translate?" );
